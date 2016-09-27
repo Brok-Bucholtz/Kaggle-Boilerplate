271d5ded3d169146311be11f6e63742f3aa862a4
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-09-26 17:56:36 -0700

    Add gradient boosting

--- model/model.py
+++ model/model.py
@@ -1,4 +1,5 @@
 from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 
 
 def classifiers(model):
@@ -6,8 +7,11 @@ def classifiers(model):
         'random_forest': {
             'name': 'sklearn-random_forest_classifier',
             'model':  RandomForestClassifier,
-            'parameters': {}}
-    }
+            'parameters': {}},
+        'gradient_boosting': {
+            'name': 'xgboost-xgb_classifier',
+            'model': XGBClassifier,
+            'parameters': {}}}
 
     if model == 'all':
         return clfs.values()
